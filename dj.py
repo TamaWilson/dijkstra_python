14c9671d5a2191f9515904585cf68a12cc5cd04a
TamaWilson <wilson_tama@hotmail.com>
2016-08-25 20:56:09 -0300

    Add files via upload

---
+++ dj.py
@@ -0,0 +1,55 @@
+grafo = { "A" : { "B" : 1, "C":2 },
+          "B" : { "D":2, "E":4 },
+          "C" : { "E":2 },
+          "D" : { "F": 6 },
+          "E" : { "F": 2 },
+          "F" : { }
+          }
+
+grafo2 = {  "D" : { "A": 4, "H": 1 },
+            "A" : { "H": 10, "E": 1 },
+            "H" : { "E": 5, "I": 9 },
+            "E" : { "F" : 3 },
+            "I" : { "J" : 2 },
+            "F" : { "I" : 1, "G": 7, "B": 1, "C": 3 },
+            "G" : { },
+            "J" : { "G" : 1 },
+            "B" : { "C" : 2 },
+            "C" : { } }
+            
+
+    
+
+def dijkstra(grafo, origem):
+
+    controle = { }
+    distanciaAtual = { }
+    noAtual = { }
+    naoVisitados = []
+    atual = origem
+    noAtual[atual] = 0
+
+    
+    for vertice in grafo.keys():
+        naoVisitados.append(vertice) #inclui os vertices nos não visitados    
+        distanciaAtual[vertice] = float('inf') #inicia os vertices como infinito
+
+    distanciaAtual[atual] =0
+
+    naoVisitados.remove(atual)
+
+    while naoVisitados:
+        for vizinho, peso in grafo[atual].items():
+             pesoCalc = peso + noAtual[atual]
+             if distanciaAtual[vizinho] == float("inf") or distanciaAtual[vizinho] > pesoCalc:
+                 distanciaAtual[vizinho] = pesoCalc
+                 controle[vizinho] = distanciaAtual[vizinho]
+
+        if controle == {} : break    
+        minVizinho = min(controle.items(), key=lambda x: x[1]) #seleciona o menor vizinho
+        atual=minVizinho[0]
+        noAtual[atual] = minVizinho[1]
+        naoVisitados.remove(atual)
+        del controle[atual]
+
+    print(distanciaAtual)
