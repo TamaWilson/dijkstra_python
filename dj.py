fd57c20d0f705a523d03d198851ea0571524f558
TamaWilson <wilson_tama@hotmail.com>
2016-09-05 15:00:12 -0300

    Update dj.py

--- dj.py
+++ dj.py
@@ -2,7 +2,7 @@ grafo = { "A" : { "B" : 1, "C":2 },
           "B" : { "D":2, "E":4 },
           "C" : { "E":2 },
           "D" : { "F": 6 },
-          "E" : { "F": 2 },
+          "E" : { "F": 7 },
           "F" : { }
           }
 
@@ -20,7 +20,7 @@ grafo2 = {  "D" : { "A": 4, "H": 1 },
 
     
 
-def dijkstra(grafo, origem):
+def dijkstra(grafo, origem): #retorna a menor distancia de um dado nó para todos os outros possíveis.
 
     controle = { }
     distanciaAtual = { }
@@ -53,3 +53,47 @@ def dijkstra(grafo, origem):
         del controle[atual]
 
     print(distanciaAtual)
+
+
+def dijkstra_path(grafo, origem, fim): #retorna a menor distancia de um No origem até um No destino e o caminho até ele
+
+    controle = { }
+    distanciaAtual = { }
+    noAtual = { }
+    naoVisitados = []
+    atual = origem
+    noAtual[atual] = 0
+
+    
+    for vertice in grafo.keys():
+        naoVisitados.append(vertice) #inclui os vertices nos não visitados    
+        distanciaAtual[vertice] = float('inf') #inicia os vertices como infinito
+
+    distanciaAtual[atual] = [0,origem] 
+
+    naoVisitados.remove(atual)
+
+    while naoVisitados:
+        for vizinho, peso in grafo[atual].items():
+             pesoCalc = peso + noAtual[atual]
+             if distanciaAtual[vizinho] == float("inf") or distanciaAtual[vizinho][0] > pesoCalc:
+                 distanciaAtual[vizinho] = [pesoCalc,atual]
+                 controle[vizinho] = pesoCalc
+                 print(controle)
+                 
+        if controle == {} : break    
+        minVizinho = min(controle.items(), key=lambda x: x[1]) #seleciona o menor vizinho
+        atual=minVizinho[0]
+        noAtual[atual] = minVizinho[1]
+        naoVisitados.remove(atual)
+        del controle[atual]
+
+    print("A menor distância de %s atá %s é: %s" % (origem, fim, distanciaAtual[fim][0]))
+    print("O menor caminho é: %s" % printPath(distanciaAtual,origem, fim))          
+    
+
+def printPath(distancias,inicio, fim):
+        if  fim != inicio:
+            return "%s -- > %s" % (printPath(distancias,inicio, distancias[fim][1]),fim)
+        else:
+            return inicio
